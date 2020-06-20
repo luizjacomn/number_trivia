92ea1bfe7a5c0bb99740dffcf4c73adc6f9d5c9c
Luiz Jacó <luizjacomn@gmail.com>
2020-06-20 15:06:04 -0300

    Class 10 - Bloc setup and input conversion

---
+++ lib/features/number_trivia/presentation/bloc/number_trivia_bloc.dart
@@ -0,0 +1,18 @@
+import 'dart:async';
+
+import 'package:bloc/bloc.dart';
+
+import './bloc.dart';
+
+class NumberTriviaBloc extends Bloc<NumberTriviaEvent, NumberTriviaState> {
+
+  @override
+  NumberTriviaState get initialState => Empty();
+
+  @override
+  Stream<NumberTriviaState> mapEventToState(
+    NumberTriviaEvent event,
+  ) async* {
+    
+  }
+}
